2c9dfd6c132b35bcfa2a3e3b461bb060589286fc
shrotybin <18334786948@163.com>
2017-03-31 18:25:23 +0800

    加入retrofit

--- app/src/main/java/com/example/wuhuabin/new_weather/util/HttpUtil.java
+++ app/src/main/java/com/example/wuhuabin/new_weather/util/HttpUtil.java
@@ -2,7 +2,11 @@ package com.example.wuhuabin.new_weather.util;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 /**
  * Created by wuhuabin on 2017/3/22.
@@ -14,7 +18,18 @@ public class HttpUtil {
         OkHttpClient okHttpClient=new OkHttpClient();
         Request request=new Request.Builder().url(address).build();
         okHttpClient.newCall(request).enqueue(callback);
+    }
 
+    public static void sendRetrofit(String address, final Callback<String> callback){
+        Retrofit retrofit=new Retrofit.Builder().baseUrl(address).addConverterFactory(GsonConverterFactory.create()).build();
+        RequestService requestService = retrofit.create(RequestService.class);
+        final Call<String> call = requestService.sendRequset();
+        call.enqueue(callback);
+    }
+
+    public interface RequestService {
+        @GET("/")
+        Call<String> sendRequset();
     }
 
 }
